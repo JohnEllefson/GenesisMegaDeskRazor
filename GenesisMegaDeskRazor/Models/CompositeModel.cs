97003fc3dfbf0fe4cd20994926d4a5d437d8ef31
John Ellefson <johnellefson@byui.edu>
2023-11-01 17:32:02 -0500

    Added a CompositeModel which is a wrapper class for the Desk and DeskQuote models to try to access both the DeskQuotes and Desk objects within the same web page. Currently this is not working.

---
+++ GenesisMegaDeskRazor/Models/CompositeModel.cs
@@ -0,0 +1,9 @@
+﻿namespace GenesisMegaDeskRazor.Models
+{
+    public class CompositeModel
+    {
+        public Desk Desk { get; set; }
+        public DeskQuote DeskQuote { get; set; }
+        public Pages.DeskQuotes.IndexModel IndexModel { get; set; }
+    }
+}
